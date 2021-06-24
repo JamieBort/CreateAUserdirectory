8a887bd40b230895ea9872aa407cbb08e1c1be3b
Jamie Bort <jamiebort@gmail.com>
2021-06-24 10:36:46 -0400

    Clean up the main.js file.

--- main.js
+++ main.js
@@ -5,11 +5,20 @@ const data = require('./data.js');
 const app = express();
 
 // Express app engine and set.
+// NOTE: app.engine(ext, callback)
+// The ext parameter is the extension type like ejs, hbs, etc 
+// Return Value: It returns on Object.
 app.engine('mustache', mustacheExpress());
+// app.set(name, value)
 app.set('views', 'views');
+// below is saying use 'mustache' as the view engine.
 app.set('view engine', 'mustache');
 
 // Express app being used
+// NOTE: app.use(path, callback)
+// path: It is the path for which the middleware function is being called. It can be a string representing a path or path pattern or regular expression pattern to match the paths.
+// callback: It is a middleware function or a series/array of middleware functions.
+// More info here: https://stackoverflow.com/questions/11321635/nodejs-express-what-is-app-use
 app.use(express.static('public'));
 
 // Express app get.
@@ -18,27 +27,22 @@ app.get('/', function (request, response) {
     var can_see_ternary = false;
     var available = "no message"
 
-    // console.log(data.users[1].id);
-    // console.log(data.users[1].job);
-    // console.log(data.users[2].id);
-    // console.log(data.users[2].job);
-
     for (var i = 0; i < data.users.length; i++) {
         if (data.users[i].job == null) {
             data.users[i].job = "Available for hire.";
             data.users[i].company = "Available for hire.";
             status = "Now available"
         };
-        // console.log(data.users[i].id);
-        // console.log(data.users[i].job);
-        // console.log(data.users[i].company);
-
     };
     // console.log(data.users[1].id);
-    // console.log(data.users[1].job);
-    // console.log(data.users[2].id);
     // console.log(data.users[2].job);
+    // console.log(data.users[3].company);
 
+    // NOTE: The render() method renders a view.
+    // res.render(view [, locals] [, callback])
+    // Locals: It is basically an object whose properties define local variables for the view.
+    // Callback It is a callback function.
+    // Returns: It returns an Object.
     response.render('home', {
 
         // users: data.users,
@@ -48,26 +52,16 @@ app.get('/', function (request, response) {
         can_see_toggle_second: can_see_ternary ? "" : "hidden",
         status: status,
         available: status,
-
-
     })
-
-
-    // document.getElementsByTagName("p").innerHTML = "The full URL of this page is:<br>"
-    // document.getElementById("demo").innerHTML = "The full URL of this page is:<br>";
 });
 
 // Retrieving data from data.js
 // This video explains how 'robotName' below works: https://www.youtube.com/watch?v=oZGmHNZv7Sc
-app.get('/:robotName', function (request, response) { // robotName is used below by asigning it to 'username'.
+app.get('/:robotName', function (request, response) { // robotName is used below by assigning it to 'username'.
     let username = request.params.robotName; // 'username' is referenced in home.mustache.
-    // console.log("the username");
-    // console.log(username);
+    // console.log("username: ",username);
     let robot_item = null; // 'username' is referenced in home.mustache
-    // let theURL = window.location.href;
-    // let theURL = request.params.robotName;
-    // document.getElementById("demo").innerHTML = "The full URL of this page is:<br>";
-    // document.getElementsByTagName("p").innerHTML = "The full URL of this page is:<br>"
+    
     for (var i = 0; i < data.users.length; i++) {
         let item = data.users[i];
 
@@ -77,7 +71,9 @@ app.get('/:robotName', function (request, response) { // robotName is used below
         // console.log(item.job);
 
         if (item.username === username) {
-            // if(item.username==data.users[i].username){console.log(data.users[i].job);}
+            // if(item.username==data.users[i].username){
+            //     console.log(data.users[i].job);
+            // }
             // console.log(item.username);
             // console.log(data.users[i].username);
             // console.log(item.job);
@@ -87,15 +83,17 @@ app.get('/:robotName', function (request, response) { // robotName is used below
             //         response.render('profile_available', { // 'profile' is referenced in profile.mustache
             //             WhatIsThis: item // 'WhatIsThis' is referenced in profile.mustache
             //         })
-            //         robot_item = item; // asigning 'data.users[i]' to robot_item. 
+            //         robot_item = item; // assigning 'data.users[i]' to robot_item. 
 
             // }
             // else{
             // console.log(item.job);
+
+            // NOTE: See notes above about the render() method.
             response.render('profile', { // 'profile' is referenced in profile.mustache
-                WhatIsThis: item // 'WhatIsThis' is referenced in profile.mustache
+                profile_item: item // 'profile_item' is referenced in profile.mustache
             })
-            robot_item = item; // asigning 'data.users[i]' to robot_item.
+            robot_item = item; // assigning 'data.users[i]' to robot_item.
         }
 
 
@@ -105,17 +103,10 @@ app.get('/:robotName', function (request, response) { // robotName is used below
 
     //  Error Message and Console.log
     if (robot_item === null) {
-
-        response.status(404).send('That user is not found.');
-        // response.status(404).send('That user is not found.' + window.location.href);
-        // document.getElementById("demo").innerHTML = "The full URL of this page is: ";
-        // document.getElementsByTagName("p").innerHTML = "The full URL of this page is: "
-        // console.log(location.href);
-        // console.log("Hello");
-        // console.log(this.attr("href"));
-        // console.log(this.getAttribute('href'));
-        // console.log(window.location.pathname);
-        // console.log(window.location.href);
+        response.setHeader('Content-type','text/html')
+        response.status(404).send(`<p>${username}`+ ' is not found.</p><a class="home_a" href="/">Return to the Robot Employee Database</a>');
+        
+        console.log(`${username}`+ ' is not in the data.js file.');
         return;
     }
 
